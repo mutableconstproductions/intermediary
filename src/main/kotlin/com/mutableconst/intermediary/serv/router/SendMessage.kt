90dbf9825762f8c8cb2ef322f60fc0a870be0a9b
juszczakn <juszczakn@gmail.com>
2016-02-03 19:00:54 -0600

    logging working

--- src/main/kotlin/com/mutableconst/intermediary/serv/router/SendMessage.kt
+++ src/main/kotlin/com/mutableconst/intermediary/serv/router/SendMessage.kt
@@ -5,26 +5,26 @@ import com.mutableconst.intermediary.db.entity.DbMessage
 import com.mutableconst.intermediary.dto.RegisterDto
 import com.mutableconst.intermediary.dto.request.Message
 import com.mutableconst.intermediary.serv.util.JsonUtil
-import java.util.logging.Level
-import java.util.logging.Logger
+import org.slf4j.LoggerFactory
 
 
 private data class Body(val message: String)
 
 object SendMessage {
-    private val log = Logger.getLogger(SendMessage.javaClass.name)
+    private val log = LoggerFactory.getLogger(SendMessage.javaClass)
 
     fun send(client: RegisterDto, message: Message): Boolean {
         if (!DbMessage.save(message)) {
-            log.log(Level.ALL, "Message sent to invalid contact")
+            log.error("Message sent to invalid contact")
             return false
         }
         val text = JsonUtil.toJson(Body(message.text))
 
         try {
-            client.currentIp.httpPost().body(text).response()
+            val url = "http://" + client.currentIp
+            url.httpPost().body(text).response()
         } catch (e: Exception) {
-            log.log(Level.ALL, "Error sending message", e)
+            log.error("Error sending message. ", e)
             return false
         }
         return true
